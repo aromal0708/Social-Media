5f9a9d7b036c050bb7802f488c9e617f93e39b62
Aromal0708 <aromalsunil12@gmail.com>
2023-05-24 15:21:13 +0530

    Update user api corrected

--- Server/Routes/Users.js
+++ Server/Routes/Users.js
@@ -1,33 +1,33 @@
+const user = require("../Models/user");
 const User = require("../Models/user");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
 // Update User
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
+  if (req.body.userId === req.params.id || user.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password, salt);
-        console.log(req.body)
-        console.log(req.params.id)
       } catch (err) {
-        return res.status(500).json(err);
+        return res.status(404).json({
+          status: "failed",
+          message: err.message,
+        });
       }
-    } else {
-      res.status(403).send("You can Update only your Account");
     }
-    try {
-      const user = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body
-      });
-      console.log(user)
-      res.status(200).json("Account has been Updated");
-    } catch (err) {
-      return res.status(500).json(err);
+    try{
+      const user = await User.findByIdAndUpdate(req.params.id,{
+        $set:req.body
+      })
+      res.status(200).json("Acount has been Updated")
+    }catch(err){
+      res.status(404).json(err)
     }
+  } else {
+    return res.status(403).json("You can Update only on your Account");
   }
-
 });
 
 module.exports = router;
